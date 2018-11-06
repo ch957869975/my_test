51fbe0db936f7b0ff31cbbdd0e8f9fecaeb8274c
psycho <957869975@qq.com>
2018-11-07 00:13:17 +0800

    ch

--- js/lazy.js
+++ js/lazy.js
@@ -1,28 +1,23 @@
 class LazyImage {
     constructor(selector) {
       // 懒记载图片列表，将伪数组转为数组，以便可以使用数组的api
-      this.imageElements = Array.prototype.slice.call(document.querySelectorAll(selector))
+      this.imageElements = [...document.querySelectorAll(selector)]
       this.init()
     }
   
     inViewShow() {
-      let len = this.imageElements.length
-      for(let i = 0; i < len; i++) {
-        let imageElement = this.imageElements[i]
-        const rect = imageElement.getBoundingClientRect()
-        // 出现在视野的时候加载图片
-        if(rect.top < document.documentElement.clientHeight) {
-          imageElement.src = imageElement.dataset.src
-          // 移除掉已经显示的
-          this.imageElements.splice(i, 1)
-          len--
-          i--
+      this.imageElements.map((item, index) => {
+        const rect = item.getBoundingClientRect()
+        // 出现在可视区域内则加载图片
+        if (rect.top < document.documentElement.clientHeight) {
+          item.src = item.dataset.src
+          this.imageElements.splice(index, 1)
           if(this.imageElements.length === 0) {
             // 如果全部都加载完 则去掉滚动事件监听
             document.removeEventListener('scroll', this._throttleFn)
           }
         }
-      }
+      })
     }
   
     throttle(fn, delay = 15, mustRun = 30) {
